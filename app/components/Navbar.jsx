c8ade917dadbbb9162ac6511d7b552aa5605e495
Harsh Pandhe <harshpandhehome@gmail.com>
2024-05-25 20:25:33 +0530

    Base

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
+} from "@nextui-org/navbar";
+import { Button } from "@nextui-org/button";
+import { AcmeLogo } from "@/public/AcmeLogo";
+import Link from "next/link";
+
+export default function Nav() {
+    return <div>
+        <Navbar>
+            <NavbarBrand>
+                <AcmeLogo />
+                <p className="font-bold text-inherit">ACME</p>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Features
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="#" aria-current="page">
+                        Customers
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Integrations
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    </div>;
+}
